802d0e3634c84bbe0458f6abc73ee32ddcf90481
kimkwatson <kimwatson731@gmail.com>
2024-09-21 23:37:05 -0700

    added to course page

---
+++ scripts/getDates.js
@@ -0,0 +1,6 @@
+const date = new Date();
+document.getElementById("year").innerHTML = date.getFullYear();
+
+let modified = document.lastModified;
+document.getElementById("lastModified").innerHTML = modified;
+
