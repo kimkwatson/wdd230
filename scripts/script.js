6e1d5a86717bc670d5d2ed220587b63cca768c60
kimkwatson <kimwatson731@gmail.com>
2024-09-26 10:03:32 -0700

    added normalizion and dark mode

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,10 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 })
+
+const modeButton = document.querySelector('#darkButton');
+const main = document.querySelector("main");
+
+modeButton.addEventListener('click', () => {
+    main.classList.toggle('dark');
+})
