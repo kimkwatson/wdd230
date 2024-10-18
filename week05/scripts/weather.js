3d85af7402729376252215acceaef9bbe1f1c5e8
kimkwatson <kimwatson731@gmail.com>
2024-10-17 17:07:40 -0700

    added link to weather activity

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -21,10 +21,9 @@ async function apiFetch() {
 }
 
 apiFetch();
-displayResults();
 
 function displayResults(data) {
-    currentTemp.innerHTML = `${data.main[temp]}&deg;F`;
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     let desc = data.weather[0].description;
     weatherIcon.setAttribute('src', iconsrc);
