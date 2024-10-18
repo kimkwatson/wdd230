55bdacd11ce84110155839cd39af58a853917b26
kimkwatson <kimwatson731@gmail.com>
2024-10-17 19:22:08 -0700

    added changes to links.js

--- scripts/links.js
+++ scripts/links.js
@@ -12,8 +12,12 @@ const displayLinks = (weeks) => {
     const list = document.querySelector('.activites ul');
     weeks.forEach((week) =>  {
         let listItem = document.createElement('li');
-        listItem.textContent = `Week ${week.week}: ${week.links.join(',')}`;
+        let link = document.createElement('a');
+        link.textContent = week.links.title;
+        link.setAttribute('href', week.links);
+        listItem.textContent = `Week ${week.week}`;
         list.appendChild(listItem);
+        listItem.appendChild(link);
     });
 };
 
