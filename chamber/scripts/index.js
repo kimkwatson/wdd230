975e61b351f2e3278e2419e33a91cf33424ec3d8
kimkwatson <kimwatson731@gmail.com>
2024-10-29 18:33:28 -0700

    added current event

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -1,5 +1,8 @@
 // Banner
-
+const day = new Date().getDay();
+if (day !== 1 && day !== 2 && day !== 3) {
+    document.querySelector('.banner').style.display = "none";
+}
 const closeButton = document.querySelector('.close-banner');
 closeButton.addEventListener('click', () => {
     document.querySelector('.banner').style.display = "none";
@@ -11,8 +14,8 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const forecastContainer = document.querySelector('#forecast');
 
-const url = "https://api.openweathermap.org/data/2.5/onecall?lat=33.25&lon=-111.64&units=imperial&appid=811002c5f4b0fae224f775a8b2e67327";
-const forecasturl = "https://api.openweathermap.org/data/2.5/forecast?lat=33.25&lon=-111.64&appid=811002c5f4b0fae224f775a8b2e67327";
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=33.25&lon=-111.64&units=imperial&appid=a8bc13a741d274988e6ad3be4e09d187";
+const url2 = "https://api.openweathermap.org/data/2.5/forecast?lat=33.25&lon=-111.64&units=imperial&appid=a8bc13a741d274988e6ad3be4e09d187";
 
 async function apiFetch() {
     try {
@@ -21,45 +24,71 @@ async function apiFetch() {
             const data = await response.json();
             console.log(data);
             displayResults(data);
-        const response2 = await fetch(forecasturl);
-        if (response2.ok) {
-            const data2 = await response.json();
-            console.log(data);
-            displayForecast(data2.daily.slice(1, 4));
         } else {
             throw Error(await response.text());
         }
-    }
     } catch (error) {
         console.log(error);
     }
+}
 
+async function apiFetch2() {
+    try {
+        const response2 = await fetch(url2);
+        if (response2.ok) {
+            const data2 = await response2.json();
+            console.log(data2);
+            displayForecast(data2.list);
+        } else {
+            throw Error(await response2.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 apiFetch();
+apiFetch2();
 
 function displayResults(data) {
-    let desc = data.current.weather[0].description;
-    currentTemp.innerHTML = `Today in Queen Creek:\n\n${data.current.temp}&deg;F and ${desc}`;
-    const iconsrc = `https://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`;
+    let desc = data.weather[0].description;
+    currentTemp.innerHTML = `<u>Today in Queen Creek:</u>\n\n${data.main.temp}&deg;F and ${desc}`;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
 }
 
-function displayForecast(forecasts) {
+function displayForecast(forecasts) {    
     
+    const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     forecastContainer.innerHTML = '';
 
+    const seenDays = new Set();
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    tomorrow.setHours(0, 0, 0, 0);
+
     forecasts.forEach(forecast => {
-        const date = new Date(forecast.dt * 1000).toLocaleDateString();
-        const temp = `${forecast.main.temp}&deg;F`;
-        const desc = forecast.weather[0].description;
-        const iconsrc = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
-    
-        const forecastDiv = document.createElement('div');
-        forecastDiv.innerHTML = `<p>${date} ${temp} ${desc}</p><br><img src="${iconsrc}" alt="${desc}">`;
-    
-        forecastContainer.appendChild(forecastDiv);
+        const date = new Date(forecast.dt * 1000);
+        
+        if (date >= tomorrow) {
+            const dayOfWeek = date.getDay();
+        
+            if (!seenDays.has(dayOfWeek) && seenDays.size < 3) {
+                seenDays.add(dayOfWeek);
+                const day = weekday[dayOfWeek];
+                const temp = `${forecast.main.temp}&deg;F`;
+                const desc = forecast.weather[0].description;
+                const iconsrc = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
+
+                forecastContainer.innerHTML += `
+                <div class="forecast-day">
+                    <p>${day} ${temp} ${desc}</p>
+                    <img src="${iconsrc}" alt="${desc}" class="forecast-icon">
+                </div>
+                `;
+            }
+        }
     });
 }
 
